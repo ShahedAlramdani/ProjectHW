bd401d7c08624b967ce5f33c38639522a37d4084
Shahed <Shahedalramdani@gmail.com>
2025-01-30 10:45:41 -0500

    Update HW1.java
    new println added

--- src/Demo/HW1.java
+++ src/Demo/HW1.java
@@ -4,6 +4,9 @@ public class HW1 {
 	public class DemoTest3 {
 		public static void main (String args []) {
 			System.out.println("JAVA");
+						System.out.println("Shahed");
+			
+									System.out.println("Chocolate");
 			
 		}
 
